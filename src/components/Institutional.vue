86d65c3c36a9b47bc633fef3602390622097a333
fernandes-dev <eduardo.yugan@gmail.com>
2020-04-26 23:49:59 -0400

    added institutional component

---
+++ src/components/Institutional.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="logo-institutional">
+      <v-row justify="center" class="py-3">
+        <v-img max-height="150px" max-width="300px" width="100%" :src="logo"></v-img>
+      </v-row>
+    </div>
+    <v-tabs class="mb-8" show-arrows color="#217b43">
+      <v-tab
+        class="tab-name"
+        v-for="(route, i) in categories"
+        :key="i"
+        :to="route.path"
+      >{{route.name}}</v-tab>
+    </v-tabs>
+    <router-view class="col-12"></router-view>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Institutional',
+  props: {
+    logo: String,
+    categories: Array
+  }
+}
+</script>
+
+<style scoped>
+</style>
