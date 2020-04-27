900f33900251ca544eb254202435316cd8c99a05
fernandes-dev <eduardo.yugan@gmail.com>
2020-04-26 23:51:40 -0400

    removed hover

--- src/components/Mural.vue
+++ src/components/Mural.vue
@@ -1,38 +1,25 @@
 <template>
   <div class="mural-main">
     <v-row class="mural-content">
-      <v-img
-        height="100%"
-        width="100%"
-        class="mural-card"
-        v-for="(item, i) in items"
-        :key="i"
-        :src="item.img"
-      >
-        <template v-slot:placeholder>
-          <v-row class="fill-height ma-0" align="center" justify="center">
-            <v-progress-circular indeterminate color="grey lighten-5"></v-progress-circular>
-          </v-row>
-        </template>
-        <v-hover v-if="item.url.length > 2 " v-slot:default="{ hover }">
-          <v-img :src="item.img">
-            <v-expand-transition>
-              <div
-                v-if="hover"
-                class="d-flex green v-card--reveal white--text"
-                style="height: 100%;"
-              >
-                <v-btn icon v-if="item.modal" @click="changeView(item.id)">
-                  <v-icon color="white" x-large>mdi-eye</v-icon>
-                </v-btn>
-                <v-btn icon :to="item.url" v-else>
-                  <v-icon color="white" x-large>mdi-eye</v-icon>
-                </v-btn>
-              </div>
-            </v-expand-transition>
-          </v-img>
-        </v-hover>
+      <v-img class="mural-card" v-for="(item, i) in items" :key="i">
+        <v-img
+          class="image-hover"
+          v-if="item.url"
+          height="100%"
+          width="100%"
+          :src="item.img"
+          v-ripple
+        >
+          <router-link v-if="item.modal" to>
+            <v-img :src="item.img" @click="changeView"></v-img>
+          </router-link>
+          <router-link v-else :to="item.url">
+            <v-img :src="item.img"></v-img>
+          </router-link>
+        </v-img>
+        <v-img v-else height="100%" width="100%" :src="item.img"></v-img>
       </v-img>
+
       <ModalPage :idModal="idModal" @changeViewed="changeView" :viewed="viewedOrigin" />
     </v-row>
   </div>
@@ -51,19 +38,19 @@ export default {
       viewedOrigin: false,
       idModal: 0,
       items: [
-        { id: 1, img: '/img/mural/1.png', url: '#' },
+        { id: 1, img: '/img/mural/1.png' },
         { id: 2, img: '/img/mural/2.png', url: '/diagnostico-imagem' },//diagnóstico por imagem
-        { id: 3, img: '/img/mural/3.png', url: '#' },
+        { id: 3, img: '/img/mural/3.png' },
         { id: 4, img: '/img/mural/4.png', url: '######', modal: true }, //instituto de olhos
-        { id: 5, img: '/img/mural/5.png', url: '#' },
+        { id: 5, img: '/img/mural/5.png' },
         { id: 6, img: '/img/mural/6.png', url: '/endoscopia' },//endoscopia
-        { id: 7, img: '/img/mural/7.png', url: '#' },
+        { id: 7, img: '/img/mural/7.png' },
         { id: 8, img: '/img/mural/8.png', url: '/maternidade/dia-do-bebe' }, //maternidade
-        { id: 9, img: '/img/mural/9.png', url: '#' },
+        { id: 9, img: '/img/mural/9.png' },
         { id: 10, img: '/img/mural/10.png', url: '/vacinas' }, //vacinas
-        { id: 11, img: '/img/mural/11.png', url: '#' },
+        { id: 11, img: '/img/mural/11.png' },
         { id: 12, img: '/img/mural/12.png', url: '######', modal: true }, // banco de sangue
-        { id: 13, img: '/img/mural/13.png', url: '#' },
+        { id: 13, img: '/img/mural/13.png' },
         { id: 14, img: '/img/mural/14.png', url: '######', modal: true }, // medicina nuclear
       ]
     }
@@ -80,14 +67,11 @@ export default {
 
 <style scoped>
 .v-card--reveal {
+  background-color: rgba(0, 128, 0, 0.63);
   align-items: center;
   justify-content: center;
   opacity: 0.5;
   width: 100%;
-}
-
-.view-mural:hover {
-  cursor: pointer;
 }
 
 .mural-main {
