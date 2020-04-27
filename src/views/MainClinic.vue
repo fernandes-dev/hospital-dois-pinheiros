885a40fd281686ddf7116bac0d4c870d60943cc2
fernandes-dev <eduardo.yugan@gmail.com>
2020-04-26 23:54:54 -0400

    added main clinic component

---
+++ src/views/MainClinic.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <Institutional :categories="categories" :logo="logo" />
+  </v-container>
+</template>
+
+<script>
+
+import Institutional from '@/components/Institutional'
+
+export default {
+  components: {
+    Institutional
+  },
+  data: () => ({
+    logo: '/img/logo-diagn-img.png',
+    categories: [
+      { name: 'Exames', path: '/clinica-imagem/exames', img: '/img/card-clinic-img.png', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+      { name: 'Sobre', path: '/clinica-imagem/sobre', img: '/img/card-clinic-img.png', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+      { name: 'Notícias', path: '/clinica-imagem/noticias', img: '/img/card-clinic-img.png', text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.' },
+    ]
+  }),
+}
+</script>
+
+<style scoped>
+</style>
