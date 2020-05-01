60b2ba5169f06f33af8d5cfe9c87d2cc4f6e070c
fernandes-dev <eduardo.yugan@gmail.com>
2020-05-01 16:14:41 -0400

    added new dependencies

--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import VueYoutube from 'vue-youtube-embed'
 
 Vue.config.productionTip = false
+
+Vue.use(VueYoutube, {global: true})
 
 new Vue({
   router,
