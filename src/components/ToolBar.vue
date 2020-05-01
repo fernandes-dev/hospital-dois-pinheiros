b561336b2c3a5c3d3645cc4ebf94f2a0da183e2a
fernandes-dev <eduardo.yugan@gmail.com>
2020-05-01 16:17:34 -0400

    added toolbar with image

---
+++ src/components/ToolBar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="toolbar-main">
+    <v-parallax class="image-toolbar" height="200" :src="(imageProp ? imageProp : image)"></v-parallax>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    imageProp: String
+  },
+  data: () => ({
+    image: '/img/quem-somos.jpg'
+  })
+}
+</script>
+
+<style>
+.toolbar-main {
+  background-color: green;
+}
+
+.image-toolbar {
+  opacity: 0.9;
+  padding-bottom: 50px;
+}
+</style>
